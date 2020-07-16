220fdc56552c5a39a99c409473c0d1305f6701f9
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2020-07-15 21:31:41 -0300

    Using PureComponent for FunctionalComponent for even more perfomance. In essense, the view is still stateless, despite the characteristic not being enforced anymore.

--- src/containers/profile/actions.js
+++ src/containers/profile/actions.js
@@ -3,58 +3,52 @@ import { fromStoreJson } from "../../parsers/store-parser";
 
 // We can substitute these actions for React Hooks
 
-export const useCharacterEdit = (props) => {
+export const renameCharacter = () => {
+  const newName = window.prompt("Nome", "personagem");
 
-  const renameCharacter = () => {
-    const newName = window.prompt("Nome", "personagem");
-  
-    store.character.name = newName;
-  }
-  
-  const exportCharacter = () => {
-    console.log("Exporting Character");
-    const file = new Blob([JSON.stringify(store, null, 2)], {
-      type: "application/JSON"
-    });
-    const filename = `${store.character.name}_sheet.json`;
-  
-    console.log("character exported: ", JSON.stringify(store.character, null, 2));
-  
-    var a = document.createElement("a"),
-        url = URL.createObjectURL(file);
-    a.href = url;
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-    setTimeout(function() {
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    }, 0);
-  }
-  
-  const importCharacter = () => {
-    console.log("Importing Character");
-  
-    var input = document.createElement("input");
-    input.type = "file";
-    input.accept = "application/JSON"
-    input.className = "no-display";
-    document.body.appendChild(input);
-  
-    input.addEventListener("change", (ev) => {
-      const reader = new FileReader();
-      reader.readAsText(ev.target.files[0], "character");
-  
-      reader.addEventListener("load", (content) => {
-        const result = fromStoreJson(content.target.result);
-        store.character = result.character;
-        store.activeValues = result.activeValues;
-        store.modifiers = result.modifiers;
-      })
-    });
-    input.click();
-  }
-  
-  return [renameCharacter, exportCharacter, importCharacter];
+  store.character.name = newName;
 }
+  
+export const exportCharacter = () => {
+  console.log("Exporting Character");
+  const file = new Blob([JSON.stringify(store, null, 2)], {
+    type: "application/JSON"
+  });
+  const filename = `${store.character.name}_sheet.json`;
 
+  console.log("character exported: ", JSON.stringify(store.character, null, 2));
+
+  var a = document.createElement("a"),
+      url = URL.createObjectURL(file);
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(function() {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 0);
+}
+  
+export const importCharacter = () => {
+  console.log("Importing Character");
+
+  var input = document.createElement("input");
+  input.type = "file";
+  input.accept = "application/JSON"
+  input.className = "no-display";
+  document.body.appendChild(input);
+
+  input.addEventListener("change", (ev) => {
+    const reader = new FileReader();
+    reader.readAsText(ev.target.files[0], "character");
+
+    reader.addEventListener("load", (content) => {
+      const result = fromStoreJson(content.target.result);
+      store.character = result.character;
+      store.activeValues = result.activeValues;
+      store.modifiers = result.modifiers;
+    })
+  });
+  input.click();
+}
--- src/containers/profile/view.jsx
+++ src/containers/profile/view.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { store } from "../../store.js";
 import { Character } from "../../components/character.jsx";
 import { WebhookInput } from "../../components/webhook-input";
-import { useCharacterEdit } from "./actions.js";
+import { renameCharacter, exportCharacter, importCharacter } from "./actions.js";
 
-const ProfileView = (props) => {
-  const [renameCharacter, exportCharacter, importCharacter] = useCharacterEdit(props);
-
-  return (
-    <div className="app">
-      <div id="button-container card">
-        <WebhookInput />
-        <button className="main-button" onClick={renameCharacter}> Renomear Personagem </button>
-        <button className="main-button" onClick={exportCharacter}> Exportar </button>
-        <button className="main-button" onClick={importCharacter}> Importar </button>
+class ProfileView extends React.PureComponent {
+  render () {
+    return (
+      <div className="app">
+        <div id="button-container card">
+          <WebhookInput />
+          <button className="main-button" onClick={renameCharacter}> Renomear Personagem </button>
+          <button className="main-button" onClick={exportCharacter}> Exportar </button>
+          <button className="main-button" onClick={importCharacter}> Importar </button>
+        </div>
+        <Character character={store.character}  />
       </div>
-      <Character character={store.character}  />
-    </div>
-  )
+    )
+  }
 }
 
 export default ProfileView;
